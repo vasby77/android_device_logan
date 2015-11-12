068f6e4e3f7bc254cc41a194e2f8636ac33c7c86
ZIM <zim555@mail.ru>
2015-11-12 20:02:21 +0200

    Update settings

--- device_logan.mk
+++ device_logan.mk
@@ -40,8 +40,8 @@ PRODUCT_COPY_FILES += \
 #	device/samsung/logan/apk/Effem.apk:system/app/Effem.apk
 
 # Charger
-#PRODUCT_PACKAGES += \
-#	charger_res_images
+PRODUCT_PACKAGES += \
+	charger_res_images
 
 # Insecure ADBD
 ADDITIONAL_DEFAULT_PROPERTIES += \
@@ -52,7 +52,7 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
 PRODUCT_PACKAGES += \
     	make_ext4fs \
     	e2fsck \
-	setup_fs
+		setup_fs
 		
 # Usb accessory
 PRODUCT_PACKAGES += \
@@ -117,17 +117,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	
 # enable Google-specific location features,
 # like NetworkLocationProvider and LocationCollector
-#PRODUCT_PROPERTY_OVERRIDES += \
-#    ro.com.google.locationfeatures=1 \
-#    ro.com.google.networklocation=1
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1
 
 # Extended JNI checks
 # The extended JNI checks will cause the system to run more slowly, but they can spot a variety of nasty bugs 
 # before they have a chance to cause problems.
 # Default=true for development builds, set by android buildsystem.
-#PRODUCT_PROPERTY_OVERRIDES += \
-#    ro.kernel.android.checkjni=0 \
-#    dalvik.vm.checkjni=false
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.kernel.android.checkjni=0 \
+    dalvik.vm.checkjni=false
 
 # MTP
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
