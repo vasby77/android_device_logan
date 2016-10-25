0b36b3654328ce33589fe0c1ecdd73fb948b377a
vasby77 <vasby77@gmail.com>
2016-10-25 10:31:09 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,5 +1,6 @@
+LOCAL_PATH := $(call my-dir)
+
 ifeq ($(TARGET_DEVICE),logan)
 LOCAL_PATH := $(call my-dir)
-include $(all-subdir-makefiles)
-include $(CLEAR_VARS)
+include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
